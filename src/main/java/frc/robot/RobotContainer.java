8470c8c7169aff821a4dd55c31109fb7871ecea1
Max <maxa@e3roboticscenter.org>
2025-03-22 15:09:49 -0400

    Swapping Z and Twist to make robot turn with the joystick not the slider

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,7 +90,7 @@ public class RobotContainer {
                     -MathUtil.applyDeadband(
                         m_driverController.getX(), OIConstants.kDriveDeadband),
                     -MathUtil.applyDeadband(
-                        m_driverController.getZ(), OIConstants.kDriveDeadband),
+                        m_driverController.getTwist(), OIConstants.kDriveDeadband),
                     true),
             m_robotDrive));
 
