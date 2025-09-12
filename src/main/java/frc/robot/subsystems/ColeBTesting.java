d77fd4b1e7af699caf69a49dfcb621da95274bc6
Cole <cole@e3rc.org>
2025-09-11 20:02:49 -0400

    Using Github: Syncing with Git and Learning how to use new systems

---
+++ src/main/java/frc/robot/subsystems/ColeBTesting.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkClosedLoopController;
+
+public class ColeBTesting{
+
+    public class init{}
+
+    public class loop{}
+
+
+    
+}
