9e86f1a3623e8652e5f08c83432ddaa72f8e1adc
Max <maxa@e3roboticscenter.org>
2025-10-07 14:56:00 -0400

    Made new paths to be auto-pathfound to, and renamed the old path folders to just being old.

---
+++ src/main/java/frc/robot/Pathfinding.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Pathfinding {
+    
+}
