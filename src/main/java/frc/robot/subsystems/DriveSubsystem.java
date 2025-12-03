d2e20e7618b1cfe1157fc953e3dd8088de055f9f
Max <maxa@e3roboticscenter.org>
2025-12-02 19:08:00 -0500

    Deleting Limelight's API

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -33,7 +33,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants;
-import frc.robot.LimelightHelpers;
 import frc.robot.Robot;
 
 @SuppressWarnings("unused")
@@ -144,17 +143,6 @@ public class DriveSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // Update the odometry in the periodic block
-    // LimelightHelpers.SetRobotOrientation("limelight", m_gyro.getAngle(), 0.0, 0.0, 0.0, 0.0, 0.0);
-
-    // Get the pose estimate
-    // LimelightHelpers.PoseEstimate limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight");
-    // if(!Robot.isSimulation()){
-    // // Add it to your pose estimator
-    // m_odometry.addVisionMeasurement(
-    //     limelightMeasurement.pose,
-    //     limelightMeasurement.timestampSeconds);
-    // }
     m_odometry.update(
         Rotation2d.fromDegrees(m_gyro.getAngle(IMUAxis.kZ)),
         new SwerveModulePosition[] {
